5b2c28ab78a2555378f8f69e392ef5c2f98993f2
Myles181 <cyrile450@gmail.com>
2023-07-26 02:07:28 +0000

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,16 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    let task0 = false;
-    let task1 = true;
-  
-    if (trueOrFalse) {
-      let tempTask = true; // Use a different name inside the conditional block
-      let tempTask2 = false; // Use a different name inside the conditional block
-  
-      // You can choose not to assign them to the original variables if you want to keep the original output.
-      // task = tempTask;
-      // task2 = tempTask2;
-    }
-  
-    return [task0, task1];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
-  
+
+  return [task, task2];
+}  
--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,10 +1,9 @@
-export default class NeighborhoodsList {
-    constructor() {
-      this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-    }
-  
-    addNeighborhood = (newNeighborhood) => {
-      this.sanFranciscoNeighborhoods.push(newNeighborhood);
-      return this.sanFranciscoNeighborhoods;
-    };
-  }
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
