bfc15f81d971c2d205e7357b88e6fa823e39ef43
Emmanuel Cyril <cyrile450@gmail.com>
2023-08-03 10:32:02 +0000

    :white_check_mark: task_1

---
+++ 0x04-TypeScript/task_1/js/main.js
@@ -0,0 +1,16 @@
+// firstName(string) and lastName(string). These two attributes should only be modifiable when a Teacher is first initialized
+// fullTimeEmployee(boolean) this attribute should always be defined
+// yearsOfExperience(number) this attribute is optional
+// location(string) this attribute should always be defined
+// Add the possibility to add any attribute to the Object like contract(boolean) without specifying the name of the attribute
+var teacher3 = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+//   teacher3.setfullTimeEmployee();
+//   teacher3.setlocation();
+//   teacher3.setyearsOfExperience();
+console.log(teacher3);
---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,16 @@
+// firstName(string) and lastName(string). These two attributes should only be modifiable when a Teacher is first initialized
+// fullTimeEmployee(boolean) this attribute should always be defined
+// yearsOfExperience(number) this attribute is optional
+// location(string) this attribute should always be defined
+// Add the possibility to add any attribute to the Object like contract(boolean) without specifying the name of the attribute
+
+interface Teacher {
+    firstName: string;
+    lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+
+}
+
